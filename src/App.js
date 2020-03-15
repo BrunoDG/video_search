27e3bfd708036b9e6a7e38970ce759eb2c7b767e
Bruno Dias <brunodg.dev@gmail.com>
2020-03-15 15:20:45 -0300

    lacking some stuff, but the app is going well. the onVideoSelect function is still not working properly.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
 import React from 'react';
-/import logo from './logo.svg';
-import './css/App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Import from Material UI dependency
+import { Grid } from '@material-ui/core';
+
+// Import from API folder 
+import youtube from './api/youtube';
+
+// Import from Components folder
+import { SearchBar, VideoDetail, VideoList } from './components';
+
+class App extends React.Component {
+    state = {
+        videos: [],
+        selectedVideo: null,
+    }
+
+    componentDidMount() {
+        this.handleSubmit('');
+    }
+
+    onVideoSelect = (video) => {
+        this.setState({ selectedVIdeo: video });
+    }
+
+    handleSubmit = async (searchTerm) => {
+        const response = await youtube.get('search', {
+            params: { 
+                part: 'id, snippet',
+                maxResults: 5,
+                key: 'AIzaSyB5bwOG7J4bkwcfvKwhxofCkvz26z7DGt8',            
+                q: searchTerm 
+            }
+        });
+        
+        this.setState({ 
+            videos: response.data.items, 
+            selectedVideo: response.data.items[0] 
+        });
+    }
+
+    render() {
+        const { selectedVideo, videos } = this.state;
+        return (
+           <Grid justify="center" container spacing={10}>
+               <Grid item xs={12}>
+                    <Grid container spacing={10}>
+                        <Grid item xs={12}>
+                            <SearchBar onFormSubmit={this.handleSubmit} />
+                        </Grid>
+                        <Grid item xs={4}>
+                            <VideoList videos={videos} onVideoSelect={this.onVideoSelect} />
+                        </Grid>
+                        <Grid item xs={8}>
+                            <VideoDetail video={selectedVideo}/>
+                        </Grid>
+                    </Grid>
+               </Grid>
+           </Grid>
+        )
+    }
 }
 
 export default App;
